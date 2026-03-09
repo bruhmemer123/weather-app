d1fef69fd09bcae1e3e1936bbb7e2859fddd4703
Affan <afydking@gmail.com>
2026-03-10 02:34:59 +0530

    secon commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const api_key=b1f4826ad80932fc0369af79b3a89cc6
+
+navigator.geolocation.getCurrentPosition(async pos=>{
+  try {
+    const lat=pos.coords.latitude
+  const long=pos.coords.longitude
+  const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}&units=metric`)
+  const data=await res.json()
+  document.querySelector(".Temperature").textContent=`Temperature:${data.main.temp}`
+  } catch (error) {
+    console.error(error)
+  }
+  
+  
+},()=>{console.log("failure")})
+
+
+//data.coord,data.main.temp,data.name,data.sys.country,data.weather.0.main,data.weather.0.description,data.wind  
